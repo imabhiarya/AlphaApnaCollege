bbee2a2f5871a81909e1897ce95654751e81c75a
Abhishek Kumar Arya <abhishekarya16june@gmail.com>
2023-12-25 14:29:10 +0530

    Update ArrayFunction

--- ArraysJava.java
+++ ArraysJava.java
@@ -1,8 +1,18 @@
 public class ArraysJava {
+    public static void updateArray(int[] arr){
+        arr[0] = 100;
+        arr[2] = 89;
+        //print
+        for (int i : arr) {
+            System.out.print(i +",");
+        }
+    }
     public static void main(String[] args) {
         // Arrays
-        int marks[] = new int[4];
+        // int marks[] = new int[4];
         int marks1[] = {1,3,5,6};
-        
+
+        updateArray(marks1);
+
     }
 }
