5ce0c9dc7a8dd81cf09fc1865454dbad442d1c70
Abhishek Kumar Arya <abhishekarya16june@gmail.com>
2023-12-26 00:35:16 +0530

    Largest Num in Array fun

--- ArraysJava.java
+++ ArraysJava.java
@@ -17,22 +17,38 @@ public class ArraysJava {
         }
         return -1;
     }
+    // Largest Number in Array
+    public static int largestNum(int[] arr){
+        int maxVal = Integer.MIN_VALUE; // - infinity
+        for (int i = 0; i < arr.length; i++) {
+            if (maxVal < arr[i]) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
     public static void main(String[] args) {
         // Arrays
         // int marks[] = new int[4];
-        int marks1[] = {1,3,5,6};
+        // int marks1[] = {1,3,5,6};
 
         // updateArray(marks1);
 
         // ---> Linear Search
-        int[] num = {1,5,4,7,8,6,0,9,5,23,5,46,678,89};
-        int key = 46;
-        int index = linearSearch(num, key);
-        if (index == -1) {
-            System.out.println("Not Found");
-        } else{
-            System.out.println("Value is at index : " + index);
-        }
+        // int[] num = {1,5,4,7,8,6,0,9,5,23,5,46,678,89};
+        // int key = 46;
+        // int index = linearSearch(num, key);
+        // if (index == -1) {
+        //     System.out.println("Not Found");
+        // } else{
+        //     System.out.println("Value is at index : " + index);
+        // }
+
+        // --> Largest Number in an Array;
+        {
+            int number[] = {1,4,5,6,78,9,5,45,5,7,68,565,4};
+            System.out.println("Largest Number in the Given Array is : "+largestNum(number));
+        } 
 
     }
 }
