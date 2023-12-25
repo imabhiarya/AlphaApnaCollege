9eff353c1ccfa220dfe098bfb947f1ff475020ea
Abhishek Kumar Arya <abhishekarya16june@gmail.com>
2023-12-25 15:21:27 +0530

    Linear Search

--- ArraysJava.java
+++ ArraysJava.java
@@ -7,12 +7,32 @@ public class ArraysJava {
             System.out.print(i +",");
         }
     }
+
+    // Linear Search
+    public static int linearSearch(int[] arr, int val){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         // Arrays
         // int marks[] = new int[4];
         int marks1[] = {1,3,5,6};
 
-        updateArray(marks1);
+        // updateArray(marks1);
+
+        // ---> Linear Search
+        int[] num = {1,5,4,7,8,6,0,9,5,23,5,46,678,89};
+        int key = 46;
+        int index = linearSearch(num, key);
+        if (index == -1) {
+            System.out.println("Not Found");
+        } else{
+            System.out.println("Value is at index : " + index);
+        }
 
     }
 }
