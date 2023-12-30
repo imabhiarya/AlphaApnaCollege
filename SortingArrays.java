853202708766064dff425b197f4961c922653dac
Abhishek Kumar Arya <abhishekarya16june@gmail.com>
2023-12-30 17:13:09 +0530

    Selection Sort

--- SortingArrays.java
+++ SortingArrays.java
@@ -18,11 +18,34 @@ public class SortingArrays {
             System.out.print(arr[i] + " ");
         }
     }
+    // Selection Sort
+    public static void selectionSortAlgo(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Sorting Algorithms");
-        int[] arr = {5,4,1,3,2};
-        bubbleSortAlgo(arr);
-        printArr(arr);
+        {
+        // int[] arr = {5,4,1,3,2};
+        // bubbleSortAlgo(arr);
+        // printArr(arr);
+        }
+        // Selection Sort
+        {
+            int[] myArr ={3,5,7,8,4,2};
+            selectionSortAlgo(myArr);
+            printArr(myArr);
+        }
     }
 }
